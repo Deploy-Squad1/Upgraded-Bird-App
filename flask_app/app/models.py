aca26288c86b0ec87a2f11a931ba416ba9d2e0ca
d-sarapaniuk <d.sarapaniuk@gmail.com>
2026-02-07 14:38:26 +0200

    Refactored the application and added the Like feature

---
+++ flask_app/app/models.py
@@ -0,0 +1,32 @@
+from . import db, login_manager
+from flask_login import UserMixin
+from datetime import datetime
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+likes = db.Table(
+    "likes",
+    db.Column('image_id', db.Integer, db.ForeignKey('images.id')),
+    db.Column('user_id', db.Integer, db.ForeignKey('users.id'))
+)
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
+    images = db.relationship('Image', backref='author', lazy=True)
+
+
+class Image(db.Model):
+    __tablename__ = 'images'
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(255), nullable=False)
+    bird_name = db.Column(db.String(100), nullable=False)
+    location = db.Column(db.String(100), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    likes = db.relationship('User', secondary=likes, backref='liked_pictures')
+    
---
+++ flask_app/run.py
@@ -0,0 +1,7 @@
+from app import app
+
+# For now, this file is only used if you run the server with
+# python3 run.py
+
+if __name__ == '__main__':
+    app.run(debug=True)

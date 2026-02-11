29a0f7f0cfabb955c7007ecbf32bc0f911d8573b
Marian <skrukinickij027@gmail.com>
2026-02-11 16:01:03 +0200

    [DS-58] tfsate terraform-iam project connected to s3 bucket

--- terraform-infrastructure/terraform-iam/provider.tf
+++ terraform-infrastructure/terraform-iam/provider.tf
@@ -5,6 +5,13 @@ terraform {
         version = "~> 5.0"
         }
     }
+    backend "s3" {
+    bucket = "birds-app-state-marian-2026"
+    key    = "iam/terraform.tfstate"
+    region = "eu-central-1"
+    encrypt = true
+    use_lockfile = true
+  }
 }
 
 provider "aws" {
